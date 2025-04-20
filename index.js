133ea7c56daf26b7bf416fe8eb1e5e019dd916e3
Diogoolibr <diogodoss.oliveira@gmail.com>
2025-04-19 20:47:10 -0400

    test

---
+++ index.js
@@ -0,0 +1,7 @@
+const audio = document.getElementById('myAudio');
+
+// Adicionar um evento de clique ao elemento desejado (por exemplo, um botão ou a própria tela)
+document.addEventListener('click', () => {
+    // Reproduzir a música
+    audio.play();
+});
